12709a284c8b2f4e59da02c03f20283133c4cb55
dain russell <31678909+dainantonio@users.noreply.github.com>
2026-02-14 22:05:57 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // REPLACE 'repo-name' WITH YOUR ACTUAL GITHUB REPOSITORY NAME
+  // Example: if your repo is github.com/john/versed-up, this should be '/versed-up/'
+  base: '/versed-up/', 
 })
