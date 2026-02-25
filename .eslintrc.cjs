d45fbdc24e33715a7b7ecc943ed9c14c44d0380c
dain russell <31678909+dainantonio@users.noreply.github.com>
2026-02-24 19:02:19 -0500

    Add baseline ESLint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  ignorePatterns: ["dist", "node_modules"],
+  rules: {},
+};
